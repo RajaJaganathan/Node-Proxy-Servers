fd1179b1849888a4741aa5bbb7fd21567aa2d579
Raja J <raja_j@apple.com>
2017-01-09 22:35:47 +0530

    minimatch lib used for detect right path regex

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var proxy = require('http-proxy-middleware');
 var minimatch = require('minimatch');
+var request = require('request');
 
 
 //Local Manual API
@@ -15,8 +16,8 @@ var pathMap = {
     '/comments/[0-9]': './mockdata/comments_1.json',
     '/posts': './mockdata/posts.json',
     '/comments': './mockdata/comments.json',
-    '/v1/js/talent/privacyPolicy': './mockdata/comments.json',
-    '/v1/js/talent/**/roles': './mockdata/comments.json'
+    '/v1/note/noteid/comments': './mockdata/comments.json',
+    '/v1/note/noteid/**/id': './mockdata/comments.json'
 };
 
 
@@ -29,78 +30,41 @@ function getPath(url) {
     return null;
 }
 
+var localProxy = {
+    route: "/api",
+    handle: function(req, res, next) {
+        var hasMockFile = getPath(req.url);
+        if (hasMockFile) {
+            var filePath = pathMap[hasMockFile];
+            var body = JSON.stringify(JSON.parse(fs.readFileSync(filePath,
+                'utf8')));
+            res.setHeader("Content-Type", "application/json");
+            res.end(body);
+        }
+        next();
+    }
+}
+
 gulp.task('dev', function() {
     browserSync.init({
         server: {
             baseDir: ".",
             notify: true,
-            middleware: [{
-                    route: "/api",
-                    handle: function(req, res, next) {
-                        var hasMockFile = getPath(req.url);
-                        if (hasMockFile) {
-                            var filePath = pathMap[hasMockFile];
-                            var body = JSON.stringify(JSON.parse(fs.readFileSync(filePath,
-                                'utf8')));
-                            res.setHeader("Content-Type", "application/json");
-                            res.end(body);
-                        }
-                        next();
-                    }
-                },
+            middleware: [
+                localProxy,
                 require('connect-history-api-fallback')() //SPA refresh 
             ]
         }
     });
 });
 
-
-
-//Local API Proxy
-
-var localProxyMiddleware = proxy('/api', {
-    target: 'http://localhost:3000',
-    changeOrigin: true,
-    logLevel: 'debug',
-    pathRewrite: { //NOTE: ORDER IS IMPORTANT HERE
-        '/api/posts/[0-9]/edit': '/mockdata/posts_edit_1.json',
-        '/api/comments/[0-9]/edit': '/mockdata/comments_edit_1.json',
-        '/api/posts/[0-9]': '/mockdata/posts_1.json',
-        '/api/comments/[0-9]': '/mockdata/comments_1.json',
-        '/api/posts': '/mockdata/posts.json',
-        '/api/comments': '/mockdata/comments.json'
-    }
-    // pathRewrite: { //NOTE: ORDER IS NOT IMPORTANT HERE       
-    //     '^/api/posts/?(.*)': '/mockdata/posts.json',
-    //     '^/api/comments/?(.*)': '/mockdata/comments.json'
-    // }
-});
-
-gulp.task('default', function() {
-    browserSync.init({
-        server: {
-            baseDir: ".",
-            middleware: [
-                localProxyMiddleware
-            ]
-        }
-    });
-});
-
-
 //API Proxy
 
-// https://filemakerjobs-di.rno.apple.com/app/api/v1/js/talent/privacyPolicy
 var apiProxyMiddleware = proxy('/app', {
-    target: 'https://filemakerjobs-di.rno.apple.com/',
+    target: 'http://yourlocal.dev',
     changeOrigin: true,
+    secure: true,
     logLevel: 'debug',
-    pathRewrite: {
-        // '/api': ''
-    },
-    router: { // Some request to other server
-        // '/posts': 'http://www.sub.example.com/'
-    }
 });
 
 
@@ -108,8 +72,17 @@ gulp.task('server', function() {
     browserSync.init({
         server: {
             baseDir: ".",
+            proxy: {
+                target: "http://yourlocal.dev",
+                proxyReq: [
+                    function(proxyReq) {
+                        proxyReq.setHeader('X-Special-Proxy-Header', 'foobar');
+                        proxyReq.setHeader('JSESSIONID', '1ui25hps8dro613myk812p87a');
+                    }
+                ]
+            },
             middleware: [
-                apiProxyMiddleware
+                apiProxyMiddleware,
             ]
         }
     });
